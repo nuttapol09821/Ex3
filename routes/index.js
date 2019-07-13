b6f7a632f5f71706e823a0b5ee166ddb658c63f8
nuttapo puttwong <59143413@g.cmru.ac.th>
2019-07-13 16:14:08 +0700

    ฐานข้อมูล

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var contact = require('../config/database')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ยินดีต้อนรับ' });
+router.get('/', function (req, res, next) {
+  var query = "Select * from positions";
+  contact.query(query, function (console, result) {
+    res.json(result);
+  })
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('about', { title: 'ง่าวต๋าย1' });
 });
 
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function (req, res, next) {
   res.render('contact', { title: 'ง่าวต๋าย1' });
 });
 
